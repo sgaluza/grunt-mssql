fb53777847a402f2fea61639d648a452c2c53eb9
Sergey Galuza <sergey@galuza.com>
2017-06-25 12:23:51 +0300

    parameters and type mapping

--- tasks/mssql.js
+++ tasks/mssql.js
@@ -1,16 +1,25 @@
 'use strict';
 
 
-module.exports = async function (grunt) {
+module.exports = function (grunt) {
+  const TYPES = require('tedious').TYPES;
+  const typesmap = function(value){
+    if(Number.isInteger(value)){
+      return TYPES.Int;
+    }
+    return TYPES.NVarChar;
+  }
 
-  grunt.registerMultiTask('mssql', 'query to be excuted against a ms sql server', function () {
 
+
+  grunt.registerMultiTask('mssql', 'query to be excuted against a ms sql server', async function () {
     const done = this.async();
     const Connection = require('tedious').Connection;
+    
 
     const connect = async function (options) {
       return new Promise((res, rej) => {
-        const connection = new Connection(this.options);
+        const connection = new Connection(options);
         connection.on('connect', () => res(connection));
         connection.on('error', (err) => rej(err));
       })
@@ -19,21 +28,31 @@ module.exports = async function (grunt) {
     const exec = async function (connection, query) {
       const Request = require('tedious').Request;
       return new Promise((res, rej) => {
-        const request = new Request(query, function (err, rowCount, rows) {
+        const request = new Request(query.sql, function (err, rowCount) {
           if (err) return rej(err);
-          res(rows);
+          res({});
         });
+        if (query.params) {
+          for (const p of Object.keys(query.params)){
+            console.log('PARAMS: ' + p);
+            console.log(query.params);
+            request.addParameter(p, typesmap(query.params[p]),  query.params[p])
+          }
+        }
         connection.execSql(request);
       });
     }
 
+    
+
     const queries = this.data.query.constructor === Array ? this.data.query : [this.data.query];
 
     try {
-      const connection = await connect(this.options)
-      for (const query of queries)
+      for (const query of queries) {
+        const connection = await connect(this.options())
         await exec(connection, query);
-      connection.close();
+        connection.close();
+      }
       grunt.log.ok("Queries excuted!");
       done();
     }
